35c6a1628e42ba792f08961f396ceecabee3eb64
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-03-03 14:15:52 +0900

    add devise of admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
   root 'home#top'
   get '/about'=> 'home#about'
+
+devise_for :admins, :controllers => {
+  :registrations => 'admins/registrations',
+  :sessions => 'admins/sessions'
+  }
+
+devise_scope :user do
+  get "sign_in", :to => "users/sessions#new"
+  get "sign_out", :to => "users/sessions#destroy"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
