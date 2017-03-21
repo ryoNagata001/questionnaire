96e97f257b9e62b35a5582d4960ae2f2ff5c4cd5
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-03-21 16:19:03 +0900

    add jQuery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312071455) do
+ActiveRecord::Schema.define(version: 20170317085414) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 20170312071455) do
     t.datetime "updated_at",                    null: false
     t.integer  "chief_id"
     t.index ["admin_id"], name: "index_companies_on_admin_id", using: :btree
+  end
+
+  create_table "questions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.integer  "category_id"
+    t.integer  "survey_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["survey_id"], name: "index_questions_on_survey_id", using: :btree
   end
 
   create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -92,6 +101,7 @@ ActiveRecord::Schema.define(version: 20170312071455) do
 
   add_foreign_key "chats", "rooms"
   add_foreign_key "companies", "admins"
+  add_foreign_key "questions", "surveys"
   add_foreign_key "surveys", "companies"
   add_foreign_key "users", "companies"
 end
