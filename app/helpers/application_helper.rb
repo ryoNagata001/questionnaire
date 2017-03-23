02e9a45203fd15aa454a880cc56ffa91a79c2507
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-03-23 14:50:06 +0900

    add helper text_question?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def text_question?(question)
+    if question.category_id == 0 || question.category_id == 1 then
+      true
+    else
+      false
+    end
+  end
 end
