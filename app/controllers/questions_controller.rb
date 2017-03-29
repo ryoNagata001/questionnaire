e7eb4edad6a5468daf9d134238bbaa3deab719da
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-03-29 14:12:31 +0900

    add answer view and controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,7 @@ class QuestionsController < ApplicationController
     @company = Company.find(params[:company_id])
     @survey = Survey.find(params[:survey_id])
     @questions = @survey.questions
+    @answer_select = AnswerSelect.new
   end
 
   def create_answer_text
@@ -27,11 +28,26 @@ class QuestionsController < ApplicationController
   end
 
   def create_answer_select
-
+    @survey = Survey.find(params[:survey_id])
+    @question = Question.find(params[:id])
+    user_id = current_user.id
+    @answer_selects = answer_select_params
+    @answer_selects[:choice_ids].each do |choice|
+      @answer_select = AnswerSelect.create(user_id: user_id, choice_id: choice)
+      if !(@answer_select.save)
+        render :show
+      end
+    end
+  number = @survey.questions.index(@question)
+  next_question = number + 1
+  redirect_to company_survey_question_path(company_id: @survey.company_id, survey_id: @survey.id, id: @survey.questions[next_question].id)
   end
 
   private
     def answer_text_params
       params.require(:answer_text).permit(:content).merge(question_id: @question.id, user_id: current_user.id)
+    end
+    def answer_select_params
+      params.require(:answer_select).permit(choice_ids: [])
     end
 end
