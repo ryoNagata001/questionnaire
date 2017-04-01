8eef52a22f2df14d0ebacbbbb7e9e7c48b9b2b66
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-04-01 16:18:15 +0900

    do globalize

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module Questionnaire
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+  config.i18n.default_locale = :ja
   end
 end
