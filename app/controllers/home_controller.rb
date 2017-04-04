b66476e4496357e297e2c2e8d7e319bfd7cc4335
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-04-04 13:16:29 +0900

    add link to change langages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
+  before_action :set_locale
   def top
     @message = 'ようこそQuestionnaryへ'
   end
 
   def about; end
+
+  private
+
+    def set_locale
+      @locale = params[:locale]
+    end
 end
