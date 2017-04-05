901faa4886064d89f56b7eb024812e84d393202c
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-04-05 20:57:17 +0900

    change position of code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,7 @@
 class HomeController < ApplicationController
-  before_action :set_locale
   def top
     @message = 'ようこそQuestionnaryへ'
   end
 
   def about; end
-
-  private
-
-    def set_locale
-      @locale = params[:locale]
-    end
 end
