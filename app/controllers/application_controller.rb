de6f56afe64ba7b25a7e7ebc50fe720aeb148144
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-04-05 20:57:17 +0900

    change position of code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def set_locale
+    @locale = params[:locale]
+    if @locale == "en"
+      I18n.locale = :en
+    elsif @locale == "ja"
+      I18n.locale = :ja
+    end
+  end
+
 end
