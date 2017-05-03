42d51683bba23a2758aed1d6a5a897cfb114a6aa
ryo nagata <ryo.nagata.utokyo@gmail.com>
2017-05-03 21:25:10 +0900

    add confirmation

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,23 +16,34 @@ describe QuestionsController do
   let(:user){ create(:user, company: company) }
 
   describe 'GET #show' do
-    before do
-      login_user user
-      get :show, params: {id: question, company_id: company.id, survey_id: survey.id}
-    end
+    context 'user is login' do
+      before do
+        login_user user
+        get :show, params: {id: question, company_id: company.id, survey_id: survey.id}
+      end
 
-    it 'renders the :show template' do
-      expect(response).to render_template :show
-    end
+      it 'renders the :show template' do
+        expect(response).to render_template :show
+      end
 
-    it 'assign the requested question to @question' do
-      expect(assigns(:question)).to eq question
+      it 'assign the requested question to @question' do
+        expect(assigns(:question)).to eq question
+      end
+      it 'assign the requested user_survey to @user_survey' do
+        expect(assigns(:user_survey)).to eq user_survey
+      end
+      it 'assign the requested survey to @survey' do
+        expect(assigns(:survey)).to eq survey
+      end
     end
-    it 'assign the requested user_survey to @user_survey' do
-      expect(assigns(:user_survey)).to eq user_survey
-    end
-    it 'assign the requested survey to @survey' do
-      expect(assigns(:survey)).to eq survey
+    context 'admin is login' do
+      before do
+        login_admin admin
+        get :show, params: {id: question, company_id: company.id, survey_id: survey.id}
+      end
+      it 'redirect to / ' do
+        expect(response).to redirect_to root_url
+      end
     end
   end
 
